40a23db5574c5b666cb2c432923dfcf37da8cd25
Yuqing <wangyuqing.rachel@gmail.com>
2025-10-05 14:28:30 -0400

    bug fixed

--- articles-data.js
+++ articles-data.js
@@ -4,7 +4,7 @@ const articles = [
     title: "Notes on Anna Karenina I",
     category: "Books",
     date: "Oct 2025",
-    summary: "“From then on, the prince and the princess lived happily ever after.”This is the common ending shared by all fairy tales.",
-    link: "posts/markdown-template.html?post=anna-karenina1.txt"
+    summary: "“From then on, the prince and the princess lived happily ever after.” This is the common ending shared by all fairy tales.",
+    link: "markdown-template.html?post=anna-karenina1.txt"
   },
 ];
