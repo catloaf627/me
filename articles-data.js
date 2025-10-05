1b467445afca9affc96b0cf32dea5adc1b2143e0
Yuqing <wangyuqing.rachel@gmail.com>
2025-10-05 14:17:26 -0400

    Update articles-data.js

--- articles-data.js
+++ articles-data.js
@@ -5,6 +5,6 @@ const articles = [
     category: "Books",
     date: "Oct 2025",
     summary: "“From then on, the prince and the princess lived happily ever after.”This is the common ending shared by all fairy tales.",
-    link: "posts/markdown-template.html?post=anna-karenina1.md"
+    link: "posts/markdown-template.html?post=anna-karenina1.txt"
   },
 ];
